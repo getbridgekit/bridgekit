0361026ba825b710093246b7e768b76539b15256
Iva Ivanova <iiivanova@users.noreply.github.com>
2026-07-04 17:32:37 -0700

    Bump version to 0.3.9

--- bridgekit/__init__.py
+++ bridgekit/__init__.py
@@ -4,5 +4,5 @@ from .planner import plan
 from .redteam import redteam
 from .compare import compare
 
-__version__ = "0.3.8"
+__version__ = "0.3.9"
 __all__ = ["evaluate", "ask", "plan", "redteam", "compare"]
