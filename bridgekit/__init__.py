608bbf6d22368a551a6d1803ecb26b95976c7b54
Iva Ivanova <iiivanova@users.noreply.github.com>
2026-08-25 14:39:46 -0700

    Bump to 0.3.10

--- bridgekit/__init__.py
+++ bridgekit/__init__.py
@@ -5,5 +5,5 @@ from .redteam import redteam
 from .compare import compare
 from .summarize import summarize
 
-__version__ = "0.3.9"
+__version__ = "0.3.10"
 __all__ = ["evaluate", "ask", "plan", "redteam", "compare", "summarize"]
